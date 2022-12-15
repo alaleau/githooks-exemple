3674fbbd761fcd726210d1124ef28c93e2394fd1
Aymeric DKT <aymeric.laleau.partner@decathlon.com>
2022-12-15 14:52:12 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,11 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { render,screen } from "@testing-library/angular";
+
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'githooks-exemple'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('githooks-exemple');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('githooks-exemple app is running!');
+  it('should render Welcome', async () => {
+    await render(AppComponent);
+    await screen.getByText('Welcome');
   });
 });
+
